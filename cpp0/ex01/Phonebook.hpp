7a1dbe624af5bf52a836a87c8d8fbfc20d14fb8c
LuisB <luis.m.pereira.batista@gmail.com>
2025-06-13 18:44:51 +0100

    feat:classes

--- cpp0/ex01/Phonebook.hpp
+++ cpp0/ex01/Phonebook.hpp
@@ -6,7 +6,23 @@
 /*   By: lumiguel <lumiguel@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 10:47:47 by lumiguel          #+#    #+#             */
-/*   Updated: 2025/06/12 10:47:48 by lumiguel         ###   ########.fr       */
+/*   Updated: 2025/06/13 18:43:23 by lumiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+class Phonebook{
+	public:
+		
+};
+
+class Contact{
+	public:
+		char *firstname;
+		char *lastname;
+		char *nickname;
+		int number;
+		char *secret;
+		
+}
